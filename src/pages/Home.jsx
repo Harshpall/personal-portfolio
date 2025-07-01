3919a3df2a4174f918ac26bd02cc50890c15b7f0
Harsh Pal <harshkumar8725@gmail.com>
2025-07-01 13:45:57 +0530

    update utility tailwind clases and add , light & dark button toggle icon & functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+      {/* theme toggle */}
+        <ThemeToggle />
+      {/* background effect */}
+
+
+      {/* Navbar */}
+
+      {/* main contant */}
+
+      {/* footer */}
+    </div>
+  )
+}
+
+export default Home
